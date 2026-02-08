116f338476f42bbe05e4f2ee790dc286299363a3
akatowolf <akatowolf@hotmail.com>
2026-02-08 13:44:58 -0500

    configuracion repo

---
+++ clasificacion-emociones-audio/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementación de un sistema de reconocimiento automático de emociones en voz basado en el análisis de señales de audio.',
+    author='akatowolf',
+    license='',
+)
